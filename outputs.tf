bec0dbc6e695ba7968897d90c661a868cb97fad4
Mark J Menger <mark.j.menger@gmail.com>
2021-10-05 17:07:37 +0000

    eliminate unused outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 # output "as3_payload" {
-#     value = local.as3_json
+# #     value = local.as3_json
+# # }
+# output "app_payload" {
+#     value = local.apps_json
 # }
-output "app_payload" {
-    value = local.apps_json
-}
