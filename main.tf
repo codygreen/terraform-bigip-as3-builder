6e43e404f552ee8e7e3f041d9466895f0044f55d
Mark J Menger <mark.j.menger@gmail.com>
2021-10-05 17:07:14 +0000

    create hcl representation of the AS3 templates

--- main.tf
+++ main.tf
@@ -2,21 +2,98 @@ terraform {
   required_version = ">=0.13"
 }
 
-# for debug purposes 
-resource "local_file" "as3" {
-    content     = local.as3_json
-    filename = "${path.module}/as3.json"
-}
+
 
 locals {
-  apps_json = templatefile("${path.module}/templates/application.json.tpl", {
-      apps = var.applications
-  })
-  tenant_json = templatefile("${path.module}/templates/tenant.json.tpl", {
-      application_payload = local.apps_json
+  tenant = merge({class = "Tenant"},{for app in var.applications:
+              app.name => {
+                class = "Application"
+                service = {
+                  class = "Service_HTTP"
+                  virtualAddresses = app.virtualAddresses
+                  virtualPort = app.virtualPort
+                  pool = "web_pool"
+                }
+                web_pool = {
+                  class = "Pool"
+                  monitors = ["http"]
+                  members = [
+                    for pool in app.pool_members:
+                    {
+                      servicePort = pool.servicePort
+                      serverAddresses = pool.serverAddresses
+                    }
+                  ]
+                }
+              }
+            })
+
+  payload = jsonencode({
+      "$schema" = "https://raw.githubusercontent.com/F5Networks/f5-appsvcs-extension/master/schema/latest/as3-schema.json",
+      class = "AS3"
+      action = "deploy"
+      persist = true
+      declaration = {
+        class = "ADC"
+        schemaVersion = "3.0.0"
+        id = "1234556"
+        label = "some ole label"
+        remark = "a remark"
+        controls = {
+          trace = true
+        }
+        "${var.tenant_name}" = local.tenant
+      }
   })
 
-  as3_json = templatefile("${path.module}/templates/as3.json.tpl", {
-      tenant_payload = local.tenant_json
-  })
+  cleanpayload = {
+      "$schema" = "https://raw.githubusercontent.com/F5Networks/f5-appsvcs-extension/master/schema/latest/as3-schema.json",
+      class = "AS3"
+      action = "deploy"
+      persist = true
+      declaration = {
+        class = "ADC"
+        schemaVersion = "3.0.0"
+        id = "1234556"
+        label = "some ole label"
+        remark = "a remark"
+        controls = {
+          trace = true
+        }
+        "${var.tenant_name}" = merge({class = "Tenant"},{for app in var.applications:
+              app.name => {
+                class = "Application"
+                service = {
+                  class = "Service_HTTP"
+                  virtualAddresses = app.virtualAddresses
+                  virtualPort = app.virtualPort
+                  pool = "web_pool"
+                }
+                web_pool = {
+                  class = "Pool"
+                  monitors = ["http"]
+                  members = [
+                    for pool in app.pool_members:
+                    {
+                      servicePort = pool.servicePort
+                      serverAddresses = pool.serverAddresses
+                    }
+                  ]
+                }
+              }
+            })
+      }
+  }
+
+
+}
+
+output "as3" {
+  value = local.cleanpayload
+}
+
+# for debug purposes 
+resource "local_file" "as3" {
+    content     = jsonencode(local.cleanpayload)
+    filename = "${path.module}/as3.json"
 }
